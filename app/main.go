f7332ee9e68e9040f253fef97a7b79a8dd0e1b5b
Milind Adpaiker <milind.adpaiker@gmail.com>
2025-04-12 17:09:14 -0400

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -11,9 +11,6 @@ import (
 	"strings"
 )
 
-// Ensures gofmt doesn't remove the "net" and "os" imports above (feel free to remove this!)
-var _ = net.Listen
-var _ = os.Exit
 
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -31,48 +28,106 @@ func main() {
 		if err != nil {
 			fmt.Println("Error accepting connection: ", err.Error())
 			os.Exit(1)
-		}
-		
+		}		
 		go handleConnection(conn)
 	}
-	
 
 }
+
+func handleConnection(conn net.Conn) {
+	status := "200 OK"
+	contentLength := 0
+	contentType := "text/plain"
+	body := ""
+	defer conn.Close()
+	
+	//Read client request
+	req, err := NewRequest(conn)
+	if err != nil{
+		status = HTTPStatus[400]
+	}
+	
+	
+	//Path based routing and response handling
+ 	if strings.EqualFold(req.Path, "/"){
+		status = HTTPStatus[200]
+	}else if strings.HasPrefix(req.Path, "/echo/"){
+		echoResponse := strings.SplitN(req.Path, "/", 3)
+		contentLength = len(echoResponse[2])
+		body = echoResponse[2]
+	}else if strings.EqualFold(req.Path, "/user-agent"){
+		if uagent, exists := req.Headers["User-Agent"]; exists && len(uagent) > 0{
+			contentLength = len(uagent[0])
+			body = uagent[0]			
+		}
+	}else{
+		status = HTTPStatus[404]
+	}
+
+	//send response
+	rsp  := fmt.Sprintf(
+		"HTTP/1.1 %s\r\n" +
+		"Content-Type: %s\r\n" +
+		"Content-Length: %d\r\n\r\n" +
+		"%s", status, contentType, contentLength,body,
+	)	
+	fmt.Println("<==")
+	fmt.Println(rsp)
+	_, err = conn.Write([]byte(rsp))
+	if err != nil{
+		fmt.Printf("Error reading headers from client request %s", err.Error())
+		return
+	}	
+}
+
 
 func clean(input []byte)string{
 	output := string(input[:max(len(input)-2, 0)])
 	return output
 }
 
-func handleConnection(conn net.Conn) {
-	defer conn.Close()
-	headers := make(map[string][]string)
-	reader := bufio.NewReader(conn)
+var HTTPStatus = map[int]string{
+    200:  "200 OK",
+    404: "404 Not Found",
+	400: "400 Bad Request",
+}
+type Request struct{
+	Headers map[string][]string
+	Path string
+	Method string
+	Protocol string
+	Body []byte
+}
 
+func NewRequest(conn net.Conn) (*Request, error) {
+
+	reader := bufio.NewReader(conn)	
+	r := &Request{
+		Headers: make(map[string][]string),
+		Body : nil,
+	}
 	// Read request line
-	fmt.Println()
 	reqL, err := reader.ReadBytes('\n')
 	if err != nil{
-		fmt.Errorf("Error reading request line from client request %s", err.Error())
-		return
+		fmt.Printf("Error reading request line from client request %s", err.Error())
+		return nil, err
 	}
 	rq := clean(reqL)
 	requestLine := strings.Split(rq, " ")
 	if len(requestLine) != 3{
 		fmt.Printf("Invalid request line received %s\n", rq)
-		return		
+		return nil, err
 	}
 	
-	method, path, protocol := requestLine[0], requestLine[1], requestLine[2]
-	fmt.Println(method, path, protocol)
-	// Read headers
-	fmt.Println()
+	r.Method, r.Path, r.Protocol = requestLine[0], requestLine[1], requestLine[2]
+	
+	// // Read headers
 	for {
 		
 		h, err := reader.ReadBytes('\n')
 		if err != nil{
 			fmt.Printf("Error reading headers from client request %s\n", err.Error())
-			return
+			return nil, err
 		}
 		if bytes.Equal(h, []byte{'\r', '\n'}){
 			break
@@ -83,65 +138,33 @@ func handleConnection(conn net.Conn) {
 			fmt.Printf("Invalid header received %s", string(h))
 			continue
 		}
-		headers[hdr[0]] = strings.Split(strings.TrimSpace(hdr[1]), ",")
+		r.Headers[hdr[0]] = strings.Split(strings.TrimSpace(hdr[1]), ",")
 	}
-	for k, v := range headers{
+
+	value, exists := r.Headers["Content-Length"]
+	if exists{
+		cl, err := strconv.Atoi(value[0]) 
+		if err == nil && cl > 0{
+			r.Body = make([]byte, cl)
+			_, err = io.ReadFull(reader, r.Body)
+			if err != nil{
+				fmt.Printf("Error reading body from client request %v", err)
+			}		
+		}else{
+			fmt.Printf("Invalid Content-Length header received %v", err)
+		}
+	}	
+	fmt.Println("==>")
+	fmt.Println(r.Method, r.Path, r.Protocol)
+	for k, v := range r.Headers{
 		fmt.Printf("%s : " , k)
 		for _, s := range v{
 			fmt.Printf("%s", s)
 		}
 		fmt.Println()
-	}
-
-	// Read body if it exists
-	fmt.Println()
-	value, exists := headers["Content-Length"]
-	if exists{
-		cl, err := strconv.Atoi(value[0]) 
-		if err == nil && cl > 0{
-			body := make([]byte, cl)
-			_, err = io.ReadFull(reader, body)
-			if err != nil{
-				fmt.Printf("Error reading body from client request %v", err)
-			}		
-			fmt.Println(string(body[:]))
-		}else{
-			fmt.Printf("Invalid Content-Length header received %v", err)
-		}
-	}
-	fmt.Println()
-	// send response back to client
-	rsp := ""
-	if strings.EqualFold(path, "/"){
-		rsp  = "HTTP/1.1 200 OK\r\n\r\n"
-	}else if strings.HasPrefix(path, "/echo/"){
-		op := strings.SplitN(path, "/", 3)
-		rsp  = fmt.Sprintf(
-		"HTTP/1.1 200 OK" +
-		"\r\n" +
-		"Content-Type: text/plain\r\n" +
-		"Content-Length: %d\r\n\r\n" +
-		"%s", len(op[2]), op[2],
-	)
-	}else if strings.EqualFold(path, "/user-agent"){
-		if v, exists := headers["User-Agent"]; exists && len(v) > 0{
-			rsp  = fmt.Sprintf(
-				"HTTP/1.1 200 OK" +
-				"\r\n" +
-				"Content-Type: text/plain\r\n" +
-				"Content-Length: %d\r\n\r\n" +
-				"%s", len(v[0]), v[0],
-			)
-		}
-
-	}else{
-			rsp  = "HTTP/1.1 404 Not Found\r\n\r\n"
-	}
-	
-
-	_, err = conn.Write([]byte(rsp))
-	if err != nil{
-		fmt.Errorf("Error reading headers from client request %s", err.Error())
-		return
 	}	
+	fmt.Println(string(r.Body))	
+	return r, nil
 }
+
+
