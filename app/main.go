2662c973f2b68693f19b0536d3d5f25534d8dd9d
Milind Adpaiker <milind.adpaiker@gmail.com>
2025-04-11 23:05:58 -0400

    return 200 ok

--- app/main.go
+++ app/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net"
 	"os"
 )
@@ -14,17 +15,37 @@ func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
-	// Uncomment this block to pass the first stage
-	//
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
-	
-	_, err = l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	defer l.Close()
+
+	for{
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+		// conn.SetReadDeadline(time.Time{})
+		go func (conn net.Conn){
+			fmt.Println("Incoming connection")
+			defer conn.Close()
+			buf := make([]byte, 1024)
+			// conn.SetDeadline(time.Now().Add(10* time.Second))
+			n, err := conn.Read(buf)
+			if err != nil && err != io.EOF {
+				fmt.Println("Error receiving data from client: ", err.Error())
+				return
+			}
+			fmt.Println("data from client", string(buf[:n]))
+			if _, err = io.WriteString(conn, "HTTP/1.1 200 OK\r\n\r\n"); err != nil {
+				fmt.Println("Error sending data to client: ", err.Error())
+				return
+			}
+		}(conn)
 	}
+	
+
 }
